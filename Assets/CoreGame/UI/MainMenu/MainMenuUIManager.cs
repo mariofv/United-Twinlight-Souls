f17014b80c81808ecab38c879a100f8d5bfb02b3
mariofv <mariofernandez.services@gmail.com>
2022-02-20 00:04:19 +0100

    #ADD (MainMenu) Move camera to a predetermined position in MainMenu

--- Assets/CoreGame/UI/MainMenu/MainMenuUIManager.cs
+++ Assets/CoreGame/UI/MainMenu/MainMenuUIManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MainMenuUIManager : MonoBehaviour
 {
+    [SerializeField] private List<Transform> cameraTransforms;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.instance.cameraManager.mainCamera.transform.position = cameraTransforms[0].position;
+        GameManager.instance.cameraManager.mainCamera.transform.rotation = cameraTransforms[0].rotation;
     }
 
     // Update is called once per frame
