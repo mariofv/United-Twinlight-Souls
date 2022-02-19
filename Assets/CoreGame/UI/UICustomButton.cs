07962aea739fba7d5303b83a4c6401e7ced78e1d
mariofv <mariofernandez.services@gmail.com>
2022-02-19 23:32:57 +0100

    #ADD (MainMenu) Create bones of LogoScreen

---
+++ Assets/CoreGame/UI/UICustomButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UICustomButton : MonoBehaviour
+{
+    [SerializeField] private Image buttonBlur;
+    [SerializeField] private GameObject buttonGameObject;
+    [SerializeField] private float blurSpeed;
+
+    private bool selected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == buttonGameObject)
+        {
+            Color newColor = buttonBlur.color;
+            newColor.a = (Mathf.Sin(Time.time * blurSpeed) + 1) * 0.25f;
+            buttonBlur.color = newColor;
+        }
+    }
+}
