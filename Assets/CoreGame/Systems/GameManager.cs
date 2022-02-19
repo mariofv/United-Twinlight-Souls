9e82e836b5a0ef1ea81b1969d83ec5230cf930ab
mariofv <mariofernandez.services@gmail.com>
2022-02-19 14:12:20 +0100

    #ADD (Core) Create GameManager and InputManager

---
+++ Assets/CoreGame/Systems/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public InputManager inputManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/CoreGame/Systems/Input/InputManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [Header("Components")]
+    private PlayerInput playerInput;
+    private float currentRumbleDuration = 0f;
+    private float rumbleDuration = 0f;
+    private bool rumbling = false;
+
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+
+        playerInput.Combat.Movement.performed += ctx => OnMovementInput(ctx);
+        playerInput.Combat.Movement.canceled += ctx => OnMovementInput(ctx);
+        
+        if (Debug.isDebugBuild)
+        {
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rumbling)
+        {
+            currentRumbleDuration += Time.deltaTime;
+            if (currentRumbleDuration >= rumbleDuration)
+            {
+                rumbling = false;
+                
+                if (Gamepad.current != null)
+                {
+                    Gamepad.current.SetMotorSpeeds(0f, 0f);
+                }
+                
+            }
+        }
+    }
+
+    public void Rumble(float leftMotorSpeed, float rightMotorSpeed, float duration)
+    {
+        if (Gamepad.current != null)
+        {
+            Gamepad.current.SetMotorSpeeds(leftMotorSpeed, rightMotorSpeed);
+        }
+        rumbling = true;
+        rumbleDuration = duration;
+        currentRumbleDuration = 0f;
+    }
+
+    void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    public void OnMovementInput(InputAction.CallbackContext context)
+    {
+       // GameManager.instance.player.controlledCharacter.inputManager.movementVector = context.ReadValue<Vector2>();
+    }
+}
