d1fe72e4fcc9b863bd2879c8ac99f48a5a7072b0
mariofv <mariofernandez.services@gmail.com>
2022-03-08 21:54:20 +0100

    #ADD (Enemy) Add NavMeshAgent to enemy script

--- Assets/CoreGame/Systems/Enemy/Enemy.cs
+++ Assets/CoreGame/Systems/Enemy/Enemy.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField] private NavMeshAgent enemyNavMeshAgent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        enemyNavMeshAgent.SetDestination(GameManager.instance.player.Character().characterMovementManager.GetPosition());
     }
 }
