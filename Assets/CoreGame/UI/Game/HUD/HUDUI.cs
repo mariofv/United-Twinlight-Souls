e0d2325a4e847fb0252e36daa693625219f5e369
mariofv <mariofernandez.services@gmail.com>
2022-03-24 13:01:10 +0100

    #FIX (HUD) Fix HUD being shown in loading screen

--- Assets/CoreGame/UI/Game/HUD/HUDUI.cs
+++ Assets/CoreGame/UI/Game/HUD/HUDUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Tweening;
 
 public class HUDUI : UIElement
 {
@@ -25,6 +26,21 @@ public class HUDUI : UIElement
         {
             SelectIlonaPortrait();
         }
+    }
+
+    protected override void CreateShowTweens()
+    {
+        TweeningAnimation fadeAnimation = uiElementCanvasGroup.TweenFade(UISettings.GameUISettings.DISPLAY_TIME, 0f, 1f).DontKillOnEnd();
+        showTweens.Add(fadeAnimation);
+    }
+
+    protected override void CreateHideTweens()
+    {
+        TweeningAnimation fadeAnimation = uiElementCanvasGroup.TweenFade(UISettings.GameUISettings.DISPLAY_TIME, 1f, 0f).DontKillOnEnd();
+        hideTweens.Add(fadeAnimation);
+
+        TweeningAnimation disableGameObjectAnimation = uiElementCanvasGroup.gameObject.TweenDisable(UISettings.GameUISettings.DISPLAY_TIME).DontKillOnEnd();
+        hideTweens.Add(disableGameObjectAnimation);
     }
 
     public void SetHealth(float healthPercentage)
