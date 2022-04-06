1c5bf2a18e54dd30c5ec6ba3b287f37bffdcb797
strandedcosmonaut <mariofernandez.services@gmail.com>
2022-04-06 17:50:13 +0200

    #ADD (Necroplant) Add some Necroplant basic components

---
+++ Assets/CoreGame/Entities/Enemies/Necroplant/NecroplantAI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NecroplantAI : EnemyAI
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Reanimate()
+    {
+    }
+
+    protected override void UpdateAI()
+    {
+
+    }
+
+    public override void OnDeathStart()
+    {
+
+    }
+
+    public override void OnHitStart()
+    {
+
+    }
+
+    public override void OnSpawnStart()
+    {
+
+    }
+}
