27edc20dbda677f4eb5d5d05c80700fb6f18434e
strandedcosmonaut <mariofernandez.services@gmail.com>
2022-04-07 11:00:57 +0200

    #ADD (Necroplant) Start implementation of basic Necroplant behaviours

--- Assets/CoreGame/Entities/Enemies/Necroplant/NecroplantAI.cs
+++ Assets/CoreGame/Entities/Enemies/Necroplant/NecroplantAI.cs
@@ -4,38 +4,135 @@ using UnityEngine;
 
 public class NecroplantAI : EnemyAI
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum NecroplantState
     {
-
+        SPAWN,
+        TELEPORT_OUT,
+        IDLE,
+        AIMING_PLAYER,
+        SIMPLE_ATTACK,
+        SHOTGUN_ATTACK,
+        BARRAGE_ATTACK,
+        DYING
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private NecroplantState currentState;
+    private float aiTimer = 0f;
+    private bool playerInSight = false;
 
+    [Header("Components")]
+    [SerializeField] private Animator rootsAnimatorController;
+
+    [Header("Aiming State")]
+    [SerializeField] private float maxAimingTime;
+
+    void Awake()
+    {
+        playerDetectionCollider.onPlayerDetected.AddListener(OnPlayerDetected);
+        playerDetectionCollider.onPlayerLost.AddListener(OnPlayerLost);
     }
 
     public override void Reanimate()
     {
+        aiTimer = 0f;
+        currentState = NecroplantState.IDLE;
     }
 
     protected override void UpdateAI()
     {
+        switch (currentState)
+        {
+            case NecroplantState.SPAWN:
+                break;
+            case NecroplantState.TELEPORT_OUT:
+                break;
+            case NecroplantState.IDLE:
+                break;
+            case NecroplantState.AIMING_PLAYER:
+                break;
+            case NecroplantState.SIMPLE_ATTACK:
+                break;
+            case NecroplantState.SHOTGUN_ATTACK:
+                break;
+            case NecroplantState.BARRAGE_ATTACK:
+                break;
+            case NecroplantState.DYING:
+                break;
+        }
+    }
 
+    public override void OnSpawnStart()
+    {
+        currentState = NecroplantState.SPAWN;
+        enemy.TriggerAnimation("spawn");
+        enemy.SetInvincible(true);
+    }
+
+    public void OnSpawnEnd()
+    {
+        if (currentState != NecroplantState.SPAWN)
+        {
+            throw new UnityException("OnSpawnEnd was captured but Necroplant was in " + currentState + " state!");
+        }
+
+        enemy.SetInvincible(false);
+        if (!playerInSight)
+        {
+            TransitionToIdleState();
+        }
+        else
+        {
+            TransitionToAimingState();
+        }
+    }
+
+    private void TransitionToIdleState()
+    {
+        currentState = NecroplantState.IDLE;
+    }
+
+    private void TransitionToAimingState()
+    {
+        currentState = NecroplantState.AIMING_PLAYER;
+        aiTimer = 0f;
+    }
+
+    private void OnPlayerDetected()
+    {
+        playerInSight = true;
+        if (currentState == NecroplantState.IDLE)
+        {
+            TransitionToAimingState();
+        }
+    }
+
+    private void OnPlayerLost()
+    {
+        playerInSight = false;
+        if (currentState == NecroplantState.AIMING_PLAYER)
+        {
+            TransitionToIdleState();
+        }
     }
 
     public override void OnDeathStart()
     {
+        currentState = NecroplantState.DYING;
+        DisableNavMeshAgent();
+        enemy.TriggerAnimation("die");
+    }
 
+    public void OnDeathEnd()
+    {
+        if (currentState != NecroplantState.DYING)
+        {
+            throw new UnityException("OnDeathEnd called in state " + currentState);
+        }
+
+        enemy.Kill();
     }
 
     public override void OnHitStart()
-    {
-
-    }
-
-    public override void OnSpawnStart()
     {
 
     }
