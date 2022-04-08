db651db7e5cd83995d5ad505fd67eed1715a6b7c
strandedcosmonaut <mariofernandez.services@gmail.com>
2022-04-08 14:00:52 +0200

    #ADD (Necroplant) Necromuzzle effect is spawned after shooting

--- Assets/CoreGame/Entities/Enemies/Necroplant/Attack/NecroMuzzle.cs
+++ Assets/CoreGame/Entities/Enemies/Necroplant/Attack/NecroMuzzle.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class NecroMuzzle : MonoBehaviour
 {
     [SerializeField] private Transform necroMuzzleCannon;
+    [SerializeField] private List<ParticleSystem> necroMuzzleEffects;
     [SerializeField] private GameObject necroProjectilePrefab;
 
     public void Shoot(Vector3 direction)
     {
+        for (int i = 0; i < necroMuzzleEffects.Count; ++i)
+        {
+            necroMuzzleEffects[i].Play();
+        }
+
         GameObject instantiatedProjectile = Instantiate(necroProjectilePrefab);
         instantiatedProjectile.transform.position = necroMuzzleCannon.position;
         NecroProjectile instantiatedNecroProjectile = instantiatedProjectile.GetComponent<NecroProjectile>();
