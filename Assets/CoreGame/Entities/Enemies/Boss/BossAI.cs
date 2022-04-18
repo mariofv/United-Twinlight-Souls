279de322c59adb1e482ddeb52c0ce07c136e6d67
strandedcosmonaut <mariofernandez.services@gmail.com>
2022-04-18 12:39:02 +0200

    #ADD (Boss) Add Boss required components

---
+++ Assets/CoreGame/Entities/Enemies/Boss/BossAI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : EnemyAI
+{
+    public override void OnDeathStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnHitStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnSpawnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Reanimate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void UpdateAI()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/CoreGame/Systems/Enemy/EnemyAsset.cs
+++ Assets/CoreGame/Systems/Enemy/EnemyAsset.cs
@@ -10,7 +10,8 @@ public class EnemyAsset : ScriptableObject
     {
         MUSHDOOM,
         BITER,
-        NECROPLANT
+        NECROPLANT,
+        BOSS
     }
 
     public EnemyId enemyId;
