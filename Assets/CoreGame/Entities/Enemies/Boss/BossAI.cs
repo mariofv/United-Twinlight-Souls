7497db1990f51c2741858d38c0bf346921abdf4c
strandedcosmonaut <mariofernandez.services@gmail.com>
2022-04-22 12:54:25 +0200

    #ADD (Boss) Add Idle Phase 1 state behaviour

--- Assets/CoreGame/Entities/Enemies/Boss/BossAI.cs
+++ Assets/CoreGame/Entities/Enemies/Boss/BossAI.cs
@@ -13,10 +13,17 @@ public class BossAI : EnemyAI
     private int currentBossPhase = 0;
     private Transform playerTransform;
 
+    private float currentTime = 0f;
+
     [Header("Boss movement")]
     [SerializeField] private Transform bossTransform;
     [SerializeField] private float maxRotationAngle;
     [SerializeField] private float rotationSpeed;
+
+    [Header("Phase 1")]
+    [SerializeField] private float minTimeBetweenAttacks;
+    [SerializeField] private float maxTimeBetweenAttacks;
+    private float currentTimeBetweenAttacks;
 
     private void Awake()
     {
@@ -25,6 +32,7 @@ public class BossAI : EnemyAI
     private void Start()
     {
         playerTransform = GameManager.instance.player.GetControlledCharacter().transform;
+        TransitionToIdlePhase1State();
     }
 
     protected override void UpdateSpecific()
@@ -33,6 +41,7 @@ public class BossAI : EnemyAI
         {
             case BossState.IDLE_PHASE_1:
                 RotateTowardsPlayer();
+                UpdateIdlePhase1State();
                 break;
         }
     }
@@ -49,6 +58,22 @@ public class BossAI : EnemyAI
         float lookAngle = Mathf.Clamp(Vector3.SignedAngle(bossTransform.forward, lookDirection, Vector3.up), -maxRotationAngle, maxRotationAngle);
         Quaternion lookRotation = Quaternion.AngleAxis(lookAngle, Vector3.up);
         bossTransform.rotation = Quaternion.Slerp(bossTransform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void TransitionToIdlePhase1State()
+    {
+        currentBossState = BossState.IDLE_PHASE_1;
+        currentTimeBetweenAttacks = Random.Range(minTimeBetweenAttacks, maxTimeBetweenAttacks);
+        currentTime = 0f;
+    }
+
+    private void UpdateIdlePhase1State()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= currentTimeBetweenAttacks)
+        {
+            
+        }
     }
 
     public override void OnDeathStart()
