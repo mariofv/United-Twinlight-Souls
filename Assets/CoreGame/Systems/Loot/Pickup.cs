430cf78590e3088ec0c3adb990e9a20ba4fe63d8
mariofv <mariofernandez.services@gmail.com>
2022-05-16 17:59:37 +0200

    #CHANGE (Loot) LightOrbs spawn in an arch trajectory

--- Assets/CoreGame/Systems/Loot/Pickup.cs
+++ Assets/CoreGame/Systems/Loot/Pickup.cs
@@ -4,25 +4,59 @@ using UnityEngine;
 
 public abstract class Pickup : MonoBehaviour
 {
-    [SerializeField] private SphereCollider pickupCollider;
+    protected enum PickupState
+    {
+        DISABLED,
+        SPAWNING,
+        PICKEABLE,
+        DESPAWNING
+    }
+
+    [SerializeField] protected SphereCollider pickupCollider;
+    protected PickupState currentState;
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case PickupState.DISABLED:
+                break;
+            case PickupState.SPAWNING:
+                UpdateSpawningState();
+                break;
+            case PickupState.PICKEABLE:
+                UpdatePickeableState();
+                break;
+            case PickupState.DESPAWNING:
+                UpdateDespawningState();
+                break;
+        }
+    }
+
 
     private void Pick()
     {
         ApplyEffect();
-        pickupCollider.enabled = false;
+        TransitionToDespawningState();
     }
 
+    public abstract void Spawn(Vector3 position);
     protected abstract void ApplyEffect();
 
-    public virtual void Spawn(Vector3 position)
+    protected abstract void UpdateSpawningState();
+    protected abstract void UpdatePickeableState();
+    protected abstract void UpdateDespawningState();
+
+    protected abstract void TransitionToDespawningState();
+    protected void TransitionToDisabledState()
     {
-        transform.position = position;
-        pickupCollider.enabled = true;
+        pickupCollider.enabled = false;
+        currentState = PickupState.DISABLED;
     }
 
     public bool IsActive()
     {
-        return pickupCollider.enabled;
+        return currentState != PickupState.DISABLED;
     }
 
     private void OnTriggerEnter(Collider other)
